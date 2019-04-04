79cc2a03c862644918ead2ef8e006008813ee6ab
Irving Aparicio <irving.aparicio.pro@gmail.com>
2019-04-03 19:11:23 -0600

    1.1 First Spring: 1.1.1. Front-End 1.1.1.1. Design HTML and CSS. 1.1.1.2. JQuery.

---
+++ Working-UploadPhoto/UploadPhoto/UploadPhoto/Scripts/main-js.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $("#succesful").hide();
+    $("#error").hide();
+
+    $("#upload").click(function () {
+        var photo = $("#file").val();
+
+        if (photo >= 0) {
+            $("#error").show();
+            $("#succesful").hide();
+        } else {
+            $("#succesful").show();
+            $("#error").hide();
+        }
+
+        $("#picture").removeAttr( "src" );
+        $("#picture").attr( "src", photo);
+        //$("#picture").add(value,photo);
+    });
+
+    $("#file").click(function () {
+        $("#succesful").hide();
+        $("#error").hide();
+    });
+
+});
